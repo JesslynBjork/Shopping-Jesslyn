9beb7982a2ad36995067c1c2827ca1fe9d4b6cef
Jesslyn Bjork <jbjork@cnm.edu>
2021-10-05 13:52:30 -0500

    adding script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+window.onload = function () {
+    initShoppingList();
+};
+
+function initShoppingList() {
+    let form = document.getElementById("item-form");
+
+    form.addEventListener("submit", (event) => {
+        handleItemForm(event, form);
+    });
+}
+
+function handleItemForm(event, formRef) {
+    event.preventDefault();
+
+
+
+    addItemToShoppingList();
+    formRef.reset();
+
+    return false;
+}
+
+
+function addItemToShoppingList() {
+    let id = getRandomInt(0, 10000000);
+
+let itemName = document.getElementById("item-name");
+let itemAmount = document.getElementById("item-amount");
+    console.log (itemName)
+    // Creates list item html and appends page.
+    let itemHTML = createListItemHtml(itemName.value, itemAmount.value, id);
+    console.log("Item HTML: ", itemHTML);
+    let itemListRef = document.getElementById("shopping-list");
+    itemListRef.insertAdjacentHTML("afterend", itemHTML);
+
+    setDeleteButtonEvent(id);
+}
+function setDeleteButtonEvent(id) {
+    let deleteButton = document.getElementById("button"+id);
+    deleteButton.addEventListener("click", () => {
+        removeListItem(id);
+
+    });
+}
+
+function createListItemHtml(itemName, itemAmount, id) {
+    return `<li id="item${id}">
+                ${itemName} - ${itemAmount}
+                <button id="bottom${id}" type="button">Delete Item</button>
+            </li>`;
+}
+
+function removeListItem(id) {
+    let listItem = document.getElementById("item"+id);
+    listItem.parentNode.removeChild(listItem);
+}
+
+function getRandomInt(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum
+}
